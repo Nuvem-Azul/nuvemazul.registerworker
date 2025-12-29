85dc898e79bfb868dec7e0aacf29804fb0d49584
santtdev1 <nuvemazulworkspace@gmail.com>
2025-12-29 17:25:44 -0300

    Setting up the project with database connections and RabbitMQ.

--- registerwork/src/test/java/io/github/nuvemazul/registerwork/RegisterworkApplicationTests.java
+++ registerwork/src/test/java/io/github/nuvemazul/registerwork/RegisterworkApplicationTests.java
@@ -1,13 +1,13 @@
 package io.github.nuvemazul.registerwork;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RegisterworkApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+//
+	//@Test
+	//void contextLoads() {
+//}
 
 }
