5549302144e168e125d84bdda19d0efb26d8fb07
santtdev1 <nuvemazulworkspace@gmail.com>
2025-12-28 22:09:49 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ registerwork/src/main/java/io/github/nuvemazul/registerwork/RegisterworkApplication.java
@@ -0,0 +1,13 @@
+package io.github.nuvemazul.registerwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterworkApplication.class, args);
+	}
+
+}
---
+++ registerwork/src/test/java/io/github/nuvemazul/registerwork/RegisterworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nuvemazul.registerwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
